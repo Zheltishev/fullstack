52cd309bbd532e2c99b9de359a8eba2e5d8833fc
Georgii <zheltishev-georgii@mail.ru>
2025-04-12 18:42:44 +0300

    add controller connection logging

--- server/src/controller/connection/connection.controller.ts
+++ server/src/controller/connection/connection.controller.ts
@@ -1,12 +1,27 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
 import { ConnectionService } from './connection.service';
+import { Response } from 'express';
 
 @Controller('connection')
 export class ConnectionController {
   constructor(private readonly connectionService: ConnectionService) {}
 
   @Post()
-  logJson(@Body() data: any): void {
-    this.connectionService.logJson(data);
+  logJson(@Body() data: any, @Res() res: Response): void {
+    try {
+      this.connectionService.logJson(data);
+
+      res
+        .status(HttpStatus.OK)
+        .send({ message: 'Request successfully processed' });
+    } catch (error) {
+      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
+        message: 'An error occurred',
+        error:
+          error instanceof Error
+            ? error.message
+            : 'Error controller connection',
+      });
+    }
   }
 }
