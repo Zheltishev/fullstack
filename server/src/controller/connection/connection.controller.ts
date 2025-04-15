c3ac99bb4ca67066e1a54ade54e3121207df9d5f
Georgii <zheltishev-georgii@mail.ru>
2025-04-16 00:02:57 +0300

    add response to controller

--- server/src/controller/connection/connection.controller.ts
+++ server/src/controller/connection/connection.controller.ts
@@ -1,12 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Res } from '@nestjs/common';
 import { ConnectionService } from './connection.service';
+import { Response } from 'express';
 
 @Controller('connection')
 export class ConnectionController {
   constructor(private readonly connectionService: ConnectionService) {}
 
   @Post()
-  checkData(@Body() data: unknown): void {
-    this.connectionService.checkData(data);
+  checkData(@Body() data: unknown, @Res() res: Response): void {
+    this.connectionService.checkData(data, res);
   }
 }
