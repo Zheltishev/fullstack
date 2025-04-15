a4e0ab536b2004da176c2023dee1c07978ecafc0
Georgii <zheltishev-georgii@mail.ru>
2025-04-15 21:22:11 +0300

    add current time

--- server/src/controller/connection/connection.service.ts
+++ server/src/controller/connection/connection.service.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { IControllerConnectionData } from 'src/types/types';
+import { currentTime } from 'src/utils/currentTime';
 
 @Injectable()
 export class ConnectionService {
@@ -12,7 +13,7 @@ export class ConnectionService {
     console.log(`Data is IControllerConnectionData: `, data);
 
     const response = {
-      date: '2024-07-25 10:20:30',
+      date: currentTime(),
       interval: 10,
       messages: [
         {
