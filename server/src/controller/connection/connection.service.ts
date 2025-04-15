ecc3b4a946e2c6260464685f3b14235cee3f0b40
Georgii <zheltishev-georgii@mail.ru>
2025-04-15 21:04:46 +0300

    change response

--- server/src/controller/connection/connection.service.ts
+++ server/src/controller/connection/connection.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import { IControllerConnectionData } from 'src/types/types';
 
@@ -10,21 +11,19 @@ export class ConnectionService {
   activeController(data: IControllerConnectionData): any {
     console.log(`Data is IControllerConnectionData: `, data);
 
-    const controllerId = data.messages[0].id;
-
-    console.log(`controller Id: `, controllerId);
-
-    return {
+    const response = {
       date: '2024-07-25 10:20:30',
       interval: 10,
       messages: [
         {
-          id: controllerId,
+          id: data.messages[0].id,
           operation: 'set_active',
           active: 1,
           online: 1,
         },
       ],
     };
+
+    return JSON.stringify(response);
   }
 }
