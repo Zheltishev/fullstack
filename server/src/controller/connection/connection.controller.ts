8100c51dbf1dfd7edd2544a68faf8f6493257f8e
Georgii <zheltishev-georgii@mail.ru>
2025-04-15 08:48:17 +0300

    add checkData

--- server/src/controller/connection/connection.controller.ts
+++ server/src/controller/connection/connection.controller.ts
@@ -1,47 +1,12 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ConnectionService } from './connection.service';
-import { Response } from 'express';
-import { IControllerConnectionData, ISetActiveStatus } from 'src/types/types';
 
 @Controller('connection')
 export class ConnectionController {
   constructor(private readonly connectionService: ConnectionService) {}
 
-  private isControllerConnectionData(
-    data: IControllerConnectionData | ISetActiveStatus,
-  ): data is IControllerConnectionData {
-    return 'specificFieldForControllerConnectionData' in data;
-  }
-
-  private isSetActiveStatus(
-    data: IControllerConnectionData | ISetActiveStatus,
-  ): data is ISetActiveStatus {
-    return 'specificFieldForSetActiveStatus' in data;
-  }
-
   @Post()
-  logJson(
-    @Body() data: IControllerConnectionData | ISetActiveStatus,
-    @Res() res: Response,
-  ): void {
-    try {
-      if (this.isControllerConnectionData(data)) {
-        this.connectionService.logJson(data);
-      } else if (this.isSetActiveStatus(data)) {
-        console.log(data);
-      }
-
-      res
-        .status(HttpStatus.OK)
-        .send({ message: 'Request successfully processed' });
-    } catch (error) {
-      res.status(HttpStatus.INTERNAL_SERVER_ERROR).send({
-        message: 'An error occurred',
-        error:
-          error instanceof Error
-            ? error.message
-            : 'Error controller connection',
-      });
-    }
+  checkData(@Body() data: unknown): void {
+    this.connectionService.checkData(data);
   }
 }
--- server/src/controller/connection/connection.service.ts
+++ server/src/controller/connection/connection.service.ts
@@ -3,6 +3,17 @@ import { IControllerConnectionData, ISetActiveResponse } from 'src/types/types';
 
 @Injectable()
 export class ConnectionService {
+  checkData(data: unknown): ISetActiveResponse {
+    console.log(data);
+
+    return {
+      id: 123456789,
+      operation: 'set_active',
+      active: 1,
+      online: 1,
+    };
+  }
+
   logJson(data: IControllerConnectionData): ISetActiveResponse {
     const controllerIp = data.message[0].controller_ip;
 
