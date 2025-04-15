9299d192a3299cb2c6923b21298feb1fe729c543
Georgii <zheltishev-georgii@mail.ru>
2025-04-15 22:31:31 +0300

    add axios response

--- server/src/controller/connection/connection.service.ts
+++ server/src/controller/connection/connection.service.ts
@@ -1,17 +1,18 @@
-/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 import { IControllerConnectionData } from 'src/types/types';
 import { currentTime } from 'src/utils/currentTime';
 
 @Injectable()
 export class ConnectionService {
-  checkData(data: unknown): void {
-    this.activeController(data as IControllerConnectionData);
+  async checkData(data: unknown): Promise<void> {
+    await this.activeController(data as IControllerConnectionData);
   }
 
-  activeController(data: IControllerConnectionData): any {
+  async activeController(data: IControllerConnectionData): Promise<void> {
     console.log(`Data is IControllerConnectionData: `, data);
 
+    const controllerIp = `http://${data.messages[0].controller_ip}`;
     const response = {
       date: currentTime(3),
       interval: 10,
@@ -25,8 +26,13 @@ export class ConnectionService {
       ],
     };
 
-    console.log(JSON.stringify(response));
+    console.log('controllerIp: ', controllerIp);
+    console.log('response: ', JSON.stringify(response));
 
-    return JSON.stringify(response);
+    try {
+      await axios.post(controllerIp, response);
+    } catch (error) {
+      console.error('Error send response to controller', error);
+    }
   }
 }
