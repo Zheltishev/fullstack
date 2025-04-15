85a3c9776170daf32ab622454233cba6b8ec9393
Georgii <zheltishev-georgii@mail.ru>
2025-04-15 19:39:21 +0300

    add checkData method

--- server/src/controller/connection/connection.service.ts
+++ server/src/controller/connection/connection.service.ts
@@ -3,21 +3,34 @@ import { IControllerConnectionData, ISetActiveResponse } from 'src/types/types';
 
 @Injectable()
 export class ConnectionService {
-  checkData(data: unknown): ISetActiveResponse {
-    console.log(data);
-
-    return {
-      id: 123456789,
-      operation: 'set_active',
-      active: 1,
-      online: 1,
-    };
+  private isControllerConnectionData(
+    data: any,
+  ): data is IControllerConnectionData {
+    return (
+      typeof data === 'object' &&
+      data !== null &&
+      'id' in data &&
+      'operation' in data &&
+      'active' in data &&
+      'online' in data
+    );
   }
 
-  logJson(data: IControllerConnectionData): ISetActiveResponse {
-    const controllerIp = data.message[0].controller_ip;
+  checkData(data: unknown): void {
+    console.log('Check data type: ', data);
 
-    console.log('controllerIp: ', controllerIp);
+    // Проверяем, является ли data типом IControllerConnectionData
+    if (this.isControllerConnectionData(data)) {
+      // Если проверка прошла, вызываем activeController
+      const result = this.activeController(data);
+      console.log('Result from activeController: ', result);
+    } else {
+      console.log('Data is not of type IControllerConnectionData');
+    }
+  }
+
+  activeController(data: IControllerConnectionData): ISetActiveResponse {
+    console.log(`Data is IControllerConnectionData: `, data);
 
     return {
       id: 123456789,
