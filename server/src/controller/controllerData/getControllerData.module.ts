d281d1e6fad2795939674a509c35d2ad72d418a0
Georgii <zheltishev-georgii@mail.ru>
2025-04-22 23:58:41 +0300

    fix module providers getControllerData

--- server/src/controller/controllerData/getControllerData.module.ts
+++ server/src/controller/controllerData/getControllerData.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { GetControllerData } from './services/getControllerData.service';
 import { getControllerDataController } from './getControllerData.controller';
 import { ActiveControllerService } from './services/activeController.service';
+import { AnswerPingService } from './services/answerPing.service';
+import { CheckAccessService } from './services/checkAccess.service';
+import { ConfirmActivationService } from './services/confirmActivation.service';
+import { EventsHandleService } from './services/eventsHandle.service';
+import { UnknownTypeService } from './services/unknownType.service';
 
 @Module({
   controllers: [getControllerDataController],
-  providers: [GetControllerData, ActiveControllerService],
+  providers: [
+    ActiveControllerService,
+    AnswerPingService,
+    CheckAccessService,
+    ConfirmActivationService,
+    GetControllerData,
+    EventsHandleService,
+    UnknownTypeService,
+  ],
 })
 export class ConnectionModule {}
